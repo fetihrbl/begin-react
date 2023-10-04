9397a8833cd7a9233ea8371bf94ddf67b60069c3
Fatih <fatiheee@icloud.com>
2023-10-04 11:41:13 +0300

    Preparation phase is completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return (
+        <div>
+            <div className="card-group">
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                        </div>
+                </div>
+
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                        </div>
+                </div>
+                
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                        </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
